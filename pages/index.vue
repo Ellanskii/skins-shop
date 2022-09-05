0420f9037a37e3bc937f7b3fac4ad6f12a542ff0
Ellanski <nameimprobable@gmail.com>
2022-09-05 20:52:26 +0300

    initial

---
+++ pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <main>
+    <header min-h-screen>
+      <h1>
+        <p>К бою готов?</p>
+        <p>Собери коллекцию топовых скинов для красивой игры</p>
+      </h1>
+      <NuxtLink>Начать</NuxtLink>
+    </header>
+    <section>
+      <header>
+        <h2>Популярное</h2>
+      </header>
+      <ul>
+        <li v-for="n in 10">
+          <article>
+            <h3>Integrale</h3>
+          </article>
+        </li>
+      </ul>
+      <footer>
+        <NuxtLink>В магазин</NuxtLink>
+      </footer>
+    </section>
+    <Suspense>
+      
+      <template #fallback>
+        <div op50 italic>
+          <span animate-pulse>Loading...</span>
+        </div>
+      </template>
+    </Suspense>
+    <InputEntry />
+  </main>
+</template>
