acee9920a9bca8e171ae100c72f59af9a50dfaae
Elijah Ellanski <nameimprobable@gmail.com>
2022-09-15 23:59:47 +0300

    add help button

--- components/app/Help.vue
+++ components/app/Help.vue
@@ -1,9 +1,6 @@
-<script lang="ts" setup></script>
-
 <template>
-  <div>
-    Component: app/Help
-  </div>
+  <DaisyButton as="a" circle href="/faq">
+    <UnoIcon class="i-ic:outline-question-mark" h-6 w-6 />
+    <span sr-only>FAQ</span>
+  </DaisyButton>
 </template>
-
-<style scoped></style>
