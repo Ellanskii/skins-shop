e9a5136d2d44ece6525e9abf730299c3b0de3352
Elijah Ellanski <nameimprobable@gmail.com>
2022-09-15 23:07:45 +0300

    remove msw

--- plugins/msw/index.client.ts
+++
@@ -1,7 +0,0 @@
-// import { setupWorker } from 'msw'
-// import handlers from './handlers'
-
-export default defineNuxtPlugin(() => {
-  // const worker = setupWorker(...handlers)
-  // worker.start()
-})
