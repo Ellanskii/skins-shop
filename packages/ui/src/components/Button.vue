e30d91c4025526c7ebaccd9bc789f928d62b7616
Elijah Ellanski <nameimprobable@gmail.com>
2022-09-15 15:56:33 +0300

    wip

---
+++ packages/ui/src/components/Button.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+withDefaults(defineProps<{
+  as: string
+}>(), {
+  as: 'button',
+})
+</script>
+  
+<template>
+  <Component :is="as" class="btn">
+    <slot />
+  </Component>
+</template>
+
+<style>
+@import '@kidonng/daisyui/utilities/unstyled/button.css';
+</style>
+
