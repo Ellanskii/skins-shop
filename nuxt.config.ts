b7f49c0e57605222c29aee812a445499bad18d99
Elijah Ellanski <nameimprobable@gmail.com>
2022-09-15 21:39:54 +0300

    removed ui module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,7 @@ export default defineNuxtConfig({
     '@unocss/nuxt',
     '@pinia/nuxt',
     '@nuxtjs/color-mode',
-    '@kiss/ui',
+    // '@kiss/ui',
     // 'nuxt-schema-org',
     '@nuxtjs/strapi',
     '@nuxt/content',
