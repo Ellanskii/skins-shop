bd39573c3b062568ded1611e73d77e8bd449abe1
Elijah Ellanski <nameimprobable@gmail.com>
2022-09-17 20:48:01 +0300

    feat(component): add button color modifiers

--- components/daisy/Button.vue
+++ components/daisy/Button.vue
@@ -1,8 +1,15 @@
 <script lang="ts" setup>
 withDefaults(defineProps<{
   as?: string
+  // Color
   primary?: boolean
+  secondary?: boolean
   accent?: boolean
+  info?: boolean
+  success?: boolean
+  warning?: boolean
+  error?: boolean
+
   link?: boolean
   ghost?: boolean
 
@@ -19,7 +26,12 @@ withDefaults(defineProps<{
   <Component
     :is="as" class="btn" :class="{
       'btn-primary': primary,
+      'btn-secondary': secondary,
       'btn-accent': accent,
+      'btn-info': info,
+      'btn-success': success,
+      'btn-warning': warning,
+      'btn-error': error,
       'btn-link': link,
       'btn-ghost': ghost,
       'btn-block': block,
