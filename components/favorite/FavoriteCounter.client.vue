a222e93a7a0e7a117848c52192d15ee912705db8
Elijah Ellanski <nameimprobable@gmail.com>
2022-09-18 13:31:46 +0300

    feat: add favorites store

---
+++ components/favorite/FavoriteCounter.client.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+const favorite = useFavoriteStore()
+const count = computed(() => favorite.total)
+</script>
+
+<template>
+  <DaisyButton class="indicator" ghost icon="i-mi:heart text-accent">
+    <span class="indicator-item badge badge-secondary">{{ count }}</span>
+    <span sr-only md:not-sr-only>Favorites</span>
+  </DaisyButton>
+</template>
---
+++ components/favorite/FavoriteToggle.client.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+defineProps<{
+  id: number
+}>()
+
+const { has, add, remove } = useFavoriteStore()
+</script>
+
+<template>
+  <DaisyButton v-if="has(id)" square icon="i-mdi:heart" @click="remove(id)">
+    <span sr-only>Remove from favorites</span>
+  </DaisyButton>
+  <DaisyButton v-else square icon="i-mdi:heart-outline" @click="add(id)">
+    <span sr-only>Add to favorites</span>
+  </DaisyButton>
+</template>
