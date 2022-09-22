9d12710ff1b9204e87abcfaa4a58935beaa36ba9
Elijah Ellanski <nameimprobable@gmail.com>
2022-09-22 15:57:37 +0300

    shit

---
+++ components/cart/CartToggle.client.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  id: number
+}>()
+
+const { has, add, remove } = useCartStore()
+const button = computed(() => has(props.id) ? ({
+  onClick: () => remove(props.id),
+  // icon: 'i-mdi:cart-minus',
+  text: 'Remove from cart',
+}) : ({
+  onClick: () => add(props.id),
+  // icon: 'i-mdi:cart',
+  text: 'Add to cart',
+}))
+</script>
+
+<template>
+  <DButton accent block v-bind="{ ...button }">
+    <span>{{ button.text }}</span>
+  </DButton>
+</template>
---
+++ components/cart/CartToggle.server.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+defineProps<{
+  id: number
+}>()
+
+const { has, add, remove } = useCartStore()
+</script>
+
+<template>
+  <DButton block accent icon="i-mdi:cart">
+    <span>Add to cart</span>
+  </DButton>
+</template>
